5dd5a9d8d0a646e97becd4ab90006a99aca029c8
rdso1984 <rdso1984@gmail.com>
2024-04-16 01:04:40 -0300

    Alterado link par API

--- MelhorCompra/src/app/service/produto.service.ts
+++ MelhorCompra/src/app/service/produto.service.ts
@@ -13,6 +13,7 @@ export class ProdutoService {
   private urlMenorPreco:string = "https://menorpreco.notaparana.pr.gov.br/api/v1/produtos?local=6gkzqf9vb&termo=" //+ produto
   //+ "&categoria=55&offset=0&raio=2&data=-1&ordem=0"
 
+  private nomeProduto: string = ""
   private produtoData:ProdutoData | any
 
   constructor(private http:HttpClient) { }
@@ -30,9 +31,11 @@ export class ProdutoService {
       return "Nenhum produto fornecido."
     }
     */
+    this.nomeProduto = produto.nome
+    this.nomeProduto.replace(" ", "%20")
 
     this.produtoData = this.http.
-                      get<ProdutoData>(`${this.urlMenorPreco}${produto.nome}&categoria=${produto.categoria}&offset=0&raio=2&data=-1&ordem=0`)
+                      get<ProdutoData>(`${this.urlMenorPreco}${this.nomeProduto}&categoria=${produto.categoria}&offset=0&raio=2&data=-1&ordem=0`)
 
     return this.produtoData
     
